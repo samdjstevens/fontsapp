2d9962cb3e3f766b5e1375b2f854653f3a55b355
Sam Stevens <sam@ticketline.co.uk>
2015-06-20 15:40:53 +0100

    initial commit

---
+++ Fonts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fonts
+//
+//  Created by Sam Stevens on 20/06/2015.
+//  Copyright © 2015 Sam Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FontsTests/FontsTests.swift
@@ -0,0 +1,35 @@
+//
+//  FontsTests.swift
+//  FontsTests
+//
+//  Created by Sam Stevens on 20/06/2015.
+//  Copyright © 2015 Sam Stevens. All rights reserved.
+//
+
+import XCTest
+
+class FontsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
