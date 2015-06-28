894cba10fbb73752102eb21a0c92a534ed525ee5
Sam Stevens <sam@ticketline.co.uk>
2015-06-28 18:27:07 +0100

    add in font sizes view controller

--- Fonts/FontListViewController.swift
+++ Fonts/FontListViewController.swift
@@ -9,87 +9,72 @@
 import UIKit
 
 class FontListViewController: UITableViewController {
+    
+    
+    var fontNames: [String] = []
+    var showFavourites: Bool = false
+    
+    private var cellPointSize: CGFloat!
+    private let cellIdentifier = "FontName"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let preferredTableViewFont = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        cellPointSize = preferredTableViewFont.pointSize
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    // Before the view will be presented on the screen, check whether or not we
+    // are showing the Favourites list, and if we are, fetch the data and re-load
+    // so that it's always correct.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if showFavourites {
+            fontNames = FavouritesList.sharedFavouritesList.favourites
+            tableView.reloadData()
+        }
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    
+    // Helper method to get the font to use for a table row
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont {
+        let fontName = fontNames[indexPath.row]
+        return UIFont(name: fontName, size: cellPointSize)!
     }
-
+    
+    // Number of rows in section = the number of font names
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return fontNames.count
     }
-
-    /*
+    
+    // What to display in the table rows
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath)
+        cell.textLabel?.text = fontNames[indexPath.row]
+        cell.detailTextLabel?.text = fontNames[indexPath.row]
+        
         return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
+    // MARK: Navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        let tableViewCell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(tableViewCell)!
+        let font = fontForDisplay(atIndexPath: indexPath)
+        
+        let sizesVC = segue.destinationViewController as! FontSizesViewController
+        sizesVC.navigationItem.title = font.fontName
+        sizesVC.font = font
+        
     }
-    */
-
 }
---
+++ Fonts/FontSizesViewController.swift
@@ -0,0 +1,50 @@
+//
+//  FontSizesViewController.swift
+//  Fonts
+//
+//  Created by Sam Stevens on 28/06/2015.
+//  Copyright © 2015 Sam Stevens. All rights reserved.
+//
+
+import UIKit
+
+class FontSizesViewController: UITableViewController {
+    
+    var font: UIFont!
+    
+    private var pointSizes: [CGFloat] {
+        struct Constants {
+            static let pointSizes: [CGFloat] = [
+                9, 10,11, 12, 13, 14, 18, 24, 36, 48, 64, 72, 96, 144
+            ]
+        }
+        
+        return Constants.pointSizes
+    }
+    
+    private let cellIdentifier = "FontNameAndSize"
+    
+    
+    // How many rows in each section...
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pointSizes.count
+    }
+    
+    // What to show in the cells
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.font = fontForDisplay(atIndexPath: indexPath)
+        cell.textLabel?.text = font.fontName
+        cell.detailTextLabel?.text = "\(pointSizes[indexPath.row]) point"
+        
+        return cell
+        
+    }
+    
+    // Helper function to grab the right UIFont for a given indexPath (row)
+    func fontForDisplay(atIndexPath indexPath: NSIndexPath) -> UIFont {
+        let pointSize = pointSizes[indexPath.row]
+        return font.fontWithSize(pointSize)
+    }
+}
